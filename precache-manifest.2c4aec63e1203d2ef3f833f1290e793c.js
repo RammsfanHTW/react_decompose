2156c491c84389b00cbb80554fdc3d3115ed0eb2
RammsfanHTW <64034634+RammsfanHTW@users.noreply.github.com>
2020-06-25 07:46:18 +0300

    Updates

---
+++ precache-manifest.2c4aec63e1203d2ef3f833f1290e793c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221c83492cab686b466a43e3babaa849",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c5e6c0b4c26cb9ec32a",
+    "url": "./static/css/main.335493ed.chunk.css"
+  },
+  {
+    "revision": "b0dff7fd6bb18487afb8",
+    "url": "./static/js/2.0ce2c40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0ce2c40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5e6c0b4c26cb9ec32a",
+    "url": "./static/js/main.a1f6ba6b.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
